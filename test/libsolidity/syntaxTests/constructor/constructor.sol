2c01950982e34fb4932aeb69831c4557d8e4e40f
Erik Kundt <bitshift@posteo.org>
2018-07-17 22:56:03 +0200

    Merges old and new constructors syntax tests.

--- test/libsolidity/syntaxTests/constructor/constructor.sol
+++ test/libsolidity/syntaxTests/constructor/constructor.sol
@@ -1,1 +1,9 @@
-contract A { constructor() public {} }
+contract A { 
+    constructor() public {}
+}
+contract B { 
+    function B() public {}
+}
+// ----
+// SyntaxError: (62-84): Functions are not allowed to have the same name as the contract. If you intend this to be a constructor, use "constructor(...) { ... }" to define it.
+// Warning: (62-84): This declaration shadows an existing declaration.
